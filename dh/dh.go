e43e8f8068a22e01b3a00c068e670af95467195a
Evan Phoenix <evan@phx.io>
2013-07-30 09:57:44 -0600

    PEM loading and proper key derivation

--- dh/dh.go
+++ dh/dh.go
@@ -1,100 +1,213 @@
-
 package dh
 
 import (
-  "math/big"
-  "errors"
-  "math/rand"
+	"bytes"
+	"encoding/asn1"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"hash"
+	"io/ioutil"
+	"math/big"
+	"math/rand"
+	"reflect"
 )
 
 var bigZero = big.NewInt(0)
-var bigOne  = big.NewInt(1)
-var bigTwo  = big.NewInt(2)
+var bigOne = big.NewInt(1)
+var bigTwo = big.NewInt(2)
 
 type PublicKey struct {
-  P  *big.Int
-  G  *big.Int
-  GX *big.Int
+	P  *big.Int
+	G  *big.Int
+	GX *big.Int
 }
 
 type Group struct {
-  P *big.Int
-  G *big.Int
+	P *big.Int
+	G *big.Int
+}
+
+func (grp *Group) Print() {
+	fmt.Printf("P=%x (bits:%d)\nG=%x\n", grp.P, grp.P.BitLen(), grp.G)
+	return
 }
 
 var (
-  group1p = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF"
+	// These 2 groups are used in SSH and provide good builtin groups
 
-  group14p = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF"
+	// diffie-hellman-group1-sha1
+	group1p = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF"
+
+	// diffie-hellman-group14-sha1
+	group14p = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF"
 )
 
 func makeGroup(p string, g int64) *Group {
-  P, err := new(big.Int).SetString(p, 16)
-  if !err {
-    return nil
-  }
+	P, err := new(big.Int).SetString(p, 16)
+	if !err {
+		return nil
+	}
 
-  G := new(big.Int).SetInt64(g)
+	G := new(big.Int).SetInt64(g)
 
-  return &Group { P, G }
+	return &Group{P, G}
+}
+
+type dhparams struct {
+	P *big.Int
+	G *big.Int
+}
+
+var errBadPEM = errors.New("crypto/dh: pem file did not contain dh params")
+
+func LoadPEM(path string) (grp *Group, err error) {
+	data, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(data)
+
+	pv := reflect.New(reflect.TypeOf(Group1).Elem())
+	val := pv.Interface()
+
+	_, err = asn1.Unmarshal(block.Bytes, val)
+
+	if err != nil {
+		return nil, err
+	}
+
+	x, ok := val.(*Group)
+
+	if !ok {
+		return nil, errBadPEM
+	}
+
+	return x, nil
 }
 
 var errPublicParams = errors.New("crypto/dh: missing public parameters")
-var group1  = makeGroup(group1p, 2)
-var group14 = makeGroup(group14p, 2)
+var Group1 = makeGroup(group1p, 2)
+var Group14 = makeGroup(group14p, 2)
 
-func checkPub(pub* PublicKey) error {
-  if pub.P == nil {
-    return errPublicParams
-  }
+func checkPub(pub *PublicKey) error {
+	if pub.P == nil {
+		return errPublicParams
+	}
 
-  if pub.G == nil {
-    return errPublicParams
-  }
+	if pub.G == nil {
+		return errPublicParams
+	}
 
-  return nil
+	return nil
 }
 
 type PrivateKey struct {
-  PublicKey
-  X *big.Int
+	PublicKey
+	X *big.Int
 }
-
 
 func (priv *PrivateKey) Validate() error {
-  if err := checkPub(&priv.PublicKey); err != nil {
-    return err
-  }
+	if err := checkPub(&priv.PublicKey); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
-func MakeKey() (priv* PrivateKey) {
-  priv = new(PrivateKey)
-
-  priv.P = group1.P
-  priv.G = group1.G
-
-  q := new(big.Int).Sub(priv.P, bigOne)
-  q.Div(q, bigTwo)
-
-  rand := rand.New(rand.NewSource(0)) // TODO change me!
-
-  priv.X  = new(big.Int).Rand(rand, q)
-  priv.GX = new(big.Int).Exp(priv.G, priv.X, priv.P)
-
-  return
+type SlimPublicKey struct {
+	GX *big.Int
 }
 
-func (priv* PrivateKey) ComputeSecret(pub* PublicKey) *big.Int {
-  ret := new(big.Int)
+func (priv *PrivateKey) SlimPub() *SlimPublicKey {
+	return &SlimPublicKey{priv.GX}
+}
 
-  ret.Exp(pub.GX, priv.X, priv.P)
+func MakeKey(grp *Group) (priv *PrivateKey) {
+	priv = new(PrivateKey)
 
-  return ret
+	priv.P = grp.P
+	priv.G = grp.G
+
+	q := new(big.Int).Sub(priv.P, bigOne)
+	q.Div(q, bigTwo)
+
+	rand := rand.New(rand.NewSource(0)) // TODO change me!
+
+	priv.X = new(big.Int).Rand(rand, q)
+	priv.GX = new(big.Int).Exp(priv.G, priv.X, priv.P)
+
+	return
+}
+
+type Secret struct {
+	S big.Int
+}
+
+func (sec *Secret) Cmp(oth *Secret) int {
+	return sec.S.Cmp(&oth.S)
+}
+
+func (sec *Secret) Bytes() []byte {
+	return sec.S.Bytes()
+}
+
+var errBadCount = errors.New("crypto/dh: Count not multiple of source")
+
+func (sec *Secret) Hash(h hash.Hash) []byte {
+	return h.Sum(sec.S.Bytes())
+}
+
+// Implementation of NIST SP 800-56A, Section 5.8
+
+func (sec *Secret) DeriveKey(hf func() hash.Hash, keylen int, other []byte) []byte {
+	h := hf()
+
+	reps := keylen / h.Size()
+
+	if keylen%h.Size() != 0 {
+		reps += 1
+	}
+
+	var buf [4]byte
+
+	var dk bytes.Buffer
+
+	for counter := 1; counter <= reps; counter++ {
+		buf[0] = byte(counter >> 24)
+		buf[1] = byte(counter >> 16)
+		buf[2] = byte(counter >> 8)
+		buf[3] = byte(counter)
+
+		h.Sum(buf[:])
+		h.Sum(sec.S.Bytes())
+		dk.Write(h.Sum(other))
+
+		h = hf()
+	}
+
+	return dk.Bytes()[:keylen]
+}
+
+func (pub *PublicKey) ComputeSecret(priv *PrivateKey) *Secret {
+	ret := new(Secret)
+
+	ret.S.Exp(pub.GX, priv.X, priv.P)
+
+	return ret
+}
+
+func (pub *SlimPublicKey) ComputeSecret(priv *PrivateKey) *Secret {
+	ret := new(Secret)
+
+	ret.S.Exp(pub.GX, priv.X, priv.P)
+
+	return ret
 }
 
 // func GenerateKey(random io.Reader, bits int) (priv *PrivateKey, err error) {
-  // priv = new(PrivateKey)
+// priv = new(PrivateKey)
 
 // }
--- dh/dh_test.go
+++ dh/dh_test.go
@@ -1,34 +1,155 @@
-
 package dh
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha1"
+	"crypto/sha256"
+	"io"
 	"testing"
 )
 
 func TestKey(t *testing.T) {
-  key := MakeKey()
+	key := MakeKey(Group1)
 
-  if !key.P.ProbablyPrime(20) {
+	if !key.P.ProbablyPrime(20) {
 		t.Errorf("P is not prime")
-  }
+	}
 
-  if key.G.Cmp(key.P) == 1 {
-    t.Errorf("G is greater than P")
-  }
+	if key.G.Cmp(key.P) == 1 {
+		t.Errorf("G is greater than P")
+	}
 
-  if key.X.Cmp(key.P) == 1 {
-    t.Errorf("X is greater than P")
-  }
+	if key.X.Cmp(key.P) == 1 {
+		t.Errorf("X is greater than P")
+	}
 }
 
 func TestAlgo(t *testing.T) {
-  k1 := MakeKey()
-  k2 := MakeKey()
+	k1 := MakeKey(Group1)
+	k2 := MakeKey(Group1)
 
-  s1 := k1.ComputeSecret(&k2.PublicKey)
-  s2 := k2.ComputeSecret(&k1.PublicKey)
+	s1 := k2.ComputeSecret(k1)
+	s2 := k1.ComputeSecret(k2)
 
-  if s1.Cmp(s2) != 0 {
-    t.Errorf("Secrets not comuting")
-  }
+	if s1.Cmp(s2) != 0 {
+		t.Errorf("Secrets not comuting")
+	}
+}
+
+func TestSlimPub(t *testing.T) {
+	k1 := MakeKey(Group1)
+
+	slim := k1.SlimPub()
+
+	if k1.GX.Cmp(slim.GX) != 0 {
+		t.Errorf("Slim key doesn't have GX")
+	}
+
+	k2 := MakeKey(Group1)
+
+	s1 := slim.ComputeSecret(k2)
+	s2 := k2.SlimPub().ComputeSecret(k1)
+
+	if s1.Cmp(s2) != 0 {
+		t.Errorf("Secrets not comuting")
+	}
+}
+
+func TestAsCryptoKey(t *testing.T) {
+	k1 := MakeKey(Group1)
+
+	k2 := MakeKey(Group1)
+
+	key1 := k1.PublicKey.ComputeSecret(k2)
+
+	plaintext := []byte("some plaintext")
+
+	ckey := key1.DeriveKey(sha256.New, 16, []byte(""))
+
+	block, err := aes.NewCipher(ckey)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure.
+
+	// CTR mode is the same for both encryption and decryption, so we can
+	// also decrypt that ciphertext with NewCTR.
+
+	plaintext2 := make([]byte, len(plaintext))
+	key2 := k2.PublicKey.ComputeSecret(k1)
+
+	ckey2 := key2.DeriveKey(sha256.New, 16, []byte(""))
+
+	block2, err := aes.NewCipher(ckey2)
+
+	stream = cipher.NewCTR(block2, iv)
+	stream.XORKeyStream(plaintext2, ciphertext[aes.BlockSize:])
+
+	if bytes.Compare(plaintext, plaintext2) != 0 {
+		t.Errorf("keys didn't roundtrip encryption correctly")
+	}
+}
+
+func TestDeriveKey(t *testing.T) {
+	k1 := MakeKey(Group1)
+	k2 := MakeKey(Group1)
+
+	s := k1.PublicKey.ComputeSecret(k2)
+
+	if len(s.DeriveKey(sha256.New, 22, []byte(""))) != 22 {
+		t.Errorf("Wrong sized return slice")
+	}
+
+	if !bytes.Equal(s.DeriveKey(sha256.New, 16, []byte("")),
+		s.DeriveKey(sha256.New, 16, []byte(""))) {
+		t.Errorf("Multiple calls with same data give different keys")
+	}
+
+	if bytes.Equal(s.DeriveKey(sha256.New, 16, []byte("")),
+		s.DeriveKey(sha256.New, 32, []byte(""))) {
+		t.Errorf("keys that should be different are not")
+	}
+
+	if bytes.Equal(s.DeriveKey(sha256.New, 16, []byte("blah")),
+		s.DeriveKey(sha256.New, 16, []byte(""))) {
+		t.Errorf("keys that should be different are not")
+	}
+
+	if bytes.Equal(s.DeriveKey(sha1.New, 16, []byte("")),
+		s.DeriveKey(sha256.New, 16, []byte(""))) {
+		t.Errorf("keys that should be different are not")
+	}
+}
+
+func TestLoadPEM(t *testing.T) {
+	grp, err := LoadPEM("./dh512.pem")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if !grp.P.ProbablyPrime(20) {
+		t.Errorf("P isn't prime")
+	}
+
+	if grp.P.BitLen() != 512 {
+		t.Errorf("P isn't 512 bits")
+	}
 }
